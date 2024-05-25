57ef22183dbbda44a090bd5ca5bc068f542f52cb
Dylan Palmieri <dgpalmieri@gmail.com>
2024-05-24 17:21:38 -0800

    feat: implement 'Lox' main interface

---
+++ lox/lox.py
@@ -0,0 +1,61 @@
+"""
+This file implements the "Lox" interface from Robert Nystrom's
+"Crafting Interpreters"
+"""
+
+from pathlib import Path
+
+
+class Lox:
+    @staticmethod
+    def main(args: list[str]) -> None:
+        """
+        Runs a script if given a single file, or the
+        REPL if no args are given
+        """
+
+        if len(args) > 1:
+            print("Usage: plox [script]")
+        elif len(args) == 1:
+            Lox.__run_file__(Path(args[0]))
+        else:
+            Lox.__repl__()
+
+    @staticmethod
+    def __run_file__(file_path: Path) -> None:
+        with open(file_path) as file:
+            Lox.__run__(file.read())
+
+    @staticmethod
+    def __repl__() -> None:
+        print("-- Empty line to exit --")
+        while True:
+            line = input("> ")
+            if not line:
+                break
+            Lox.__run__(line)
+            Lox.hadError = False
+
+    @staticmethod
+    def __run__(source: str) -> None:
+        if Lox.hadError:
+            exit(65)
+
+        lexer = Lexer(source)
+        tokens = lexer.scan_tokens()
+
+        for t in tokens:
+            print(t)
+
+    @staticmethod
+    def error(line: int, message: str) -> None:
+        Lox.__report__(line, "", message)
+
+    @staticmethod
+    def __report__(line: int, where: str, message: str) -> None:
+        print(f"[line {line}] Error {where}: {message}")
+        Lox.hadError = True
+
+    # ------- Variables -------
+
+    hadError: bool = False
